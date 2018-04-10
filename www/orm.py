e45ad2b5fda99cb108bc70ec8afe07bc8b4ed0f2
zhangbo <itzhangnima@163.com>
2018-04-10 14:32:50 +0800

    第一次提交

---
+++ www/orm.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# 2018/4/10 11:50
+__Author__ = 'zb'
+
+import aiohttp,logging,aiomysql
+
+
+async def create_pool(loop,**kw):
+    logging.info('create database connection pool....')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host = kw.get('host','localhost'),
+        port = kw.get('port','3306'),
+        user = kw['user'],
+        password = kw['password'],
+        db = kw['db'],
+        charset = kw.get('charset','utf-8'),
+        autocommit = kw.get('autocommit',True),
+        maxsize = kw.get('maxsize',10),
+        minsize = kw.get('minsize',1),
+        loop = loop
+    )
+
+async def select(sql,args,size=None):
+    logging.info(sql,args)
+    global __pool
+    with (await __pool) as conn:
+        cur = await conn.cursor(aiomysql.DictCursor)
+        await cur.execute(sql.replace("?",'%s'),args or ())
+        if size:
+            rs = await cur.fetchmany(size)
+        else:
+            rs = await cur.fetchall()
+        await cur.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+async def execute(sql,args):
+    logging.info(sql)
+    with await __pool as conn:
+        try:
+            cur = await conn.cursor(aiomysql.DictCursor)
+            await cur.execute(sql.replace("?",'%s'),args or ())
+            affected = cur.rowcount()
+            await cur.close()
+        except:
+            raise
+        return affected
+
+
+
+
+
+
